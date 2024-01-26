1a0504293c2c36a3fdbb3e4e4efe5522d4dfbc16
vaishnavi <157712009+Vaishnavi-rr@users.noreply.github.com>
2024-01-26 17:41:33 +0530

    Update and rename README.md to positive and negative number.java

---
+++ positive and negative number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+public static void main (String[ ]arg){
+Scanner sc = new Scanner(System.in);
+int num=sc.nextInt();
+if(num==0){
+System.out.println("zero");
+}else{
+String result=num>0?"positive" :"Negative";
+System.out.println(result);
+}
+}
+}
